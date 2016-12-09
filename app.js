6c5b83fee402c6566e5b74e16545c62c17fe60dc
aaqib29292 <aaqib29292@gmail.com>
2016-12-09 15:24:24 +0530

    added routes for SPA and added input for city in home page

---
+++ app.js
@@ -0,0 +1,30 @@
+// Module
+
+var weatherApp = angular.module('weatherApp', ["ngResource", "ngRoute"]);
+
+
+// Routes
+
+weatherApp.config(function ($routeProvider) {
+
+  $routeProvider
+
+  .when('/', {
+    templateUrl: 'pages/home.html',
+    controller: 'homeController'
+  })
+  .when('/forecast', {
+    templateUrl: 'pages/forecast.html',
+    controller: 'forecastController'
+  })
+});
+
+// Controllers
+
+weatherApp.controller('homeController', ['$scope', function ($scope) {
+
+}]);
+
+weatherApp.controller('forecastController', ['$scope', function ($scope) {
+
+}]);
