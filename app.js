78b99dfd76b02a750286d265a4dd857cb3899779
aaqib29292 <aaqib29292@gmail.com>
2016-12-09 20:26:44 +0530

    got weather forecast for the required creating a resource and displayed on the forecast page

--- app.js
+++ app.js
@@ -17,14 +17,49 @@ weatherApp.config(function ($routeProvider) {
     templateUrl: 'pages/forecast.html',
     controller: 'forecastController'
   })
+  .when('/forecast/:days', {
+    templateUrl: 'pages/forecast.html',
+    controller: 'forecastController'
+  })
 });
+
+
+// Services
+
+weatherApp.service('cityService', function() {
+  this.city = "Hyderabad, IN"
+})
 
 // Controllers
 
-weatherApp.controller('homeController', ['$scope', function ($scope) {
+weatherApp.controller('homeController', ['$scope', 'cityService', function ($scope, cityService) {
+
+  $scope.city = cityService.city;
+
+  $scope.$watch('city', function() {
+    cityService.city = $scope.city;
+  })
 
 }]);
 
-weatherApp.controller('forecastController', ['$scope', function ($scope) {
+weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function ($scope, $resource,$routeParams, cityService) {
+
+  $scope.city = cityService.city;
+
+  $scope.days = $routeParams.days || '2';
+
+  var url = "http://api.openweathermap.org/data/2.5/forecast/daily";
+
+  $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily", {callback: "JSON_CALLBACK"}, {get: {method: "JSONP"}});
+
+  $scope.weatherResult = $scope.weatherAPI.get({q: $scope.city, cnt: $scope.days, APPID: "97ed8fa956996cbe65121c37c13f329f"});
+
+  $scope.convertToDate = function(dt) {
+    return new Date(dt * 1000);
+  }
+
+  $scope.convertToCelsius = function (degKelvin) {
+    return parseFloat(degKelvin - 273.15).toFixed(2);
+  }
 
 }]);
